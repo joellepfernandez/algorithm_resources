9e93dce64ba69fc4346c7e66fbdcb96b4a3a358e
Joelle Fernandez <joellepfernandez@gmail.com>
2020-02-10 15:11:08 -0800

    added comments and  more test cases

--- coding_questions/frequency_counters/areThereDuplicates/areThereDuplicates.test.js
+++ coding_questions/frequency_counters/areThereDuplicates/areThereDuplicates.test.js
@@ -11,3 +11,11 @@ test('duplicate number', () => {
 test('duplicate string', () => {
     expect(areThereDuplicates('a', 'b', 'c', 'a')).toBe(true);
 });
+
+test('no duplicates with various primitive data types ', () => {
+    expect(areThereDuplicates('a', 1, 'c', 2, 'hello', false)).toBe(false);
+});
+
+test('duplicates with various primitive data types ', () => {
+    expect(areThereDuplicates('a', 1, false, 'c', 2, 'hello', true, false)).toBe(true);
+});
