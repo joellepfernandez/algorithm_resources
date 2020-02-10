0914189344d5fd8d0c62d0b68aa61424655d9442
Joelle Fernandez <joellepfernandez@gmail.com>
2020-02-10 14:56:02 -0800

    added additional comments and tests

--- coding_questions/frequency_counters/sameFrequency/sameFrequency.js
+++ coding_questions/frequency_counters/sameFrequency/sameFrequency.js
@@ -1,7 +1,9 @@
 function sameFrequency(string1, string2) {
+    // Accounts for two string of different lengths
     if (string1.length !== string2.length) {
         return false;
     }
+    // Counts the frequency of both string1 and string2
     let frequencyCounter1 = {};
     let frequencyCounter2 = {};
     for (let letter of string1.split('')) {
@@ -10,8 +12,9 @@ function sameFrequency(string1, string2) {
     for (let letter of string2.split('')) {
         frequencyCounter2[letter] = (frequencyCounter2[letter] || 0) + 1;
     }
-    for (let key in frequencyCounter1){
-        if(!(key in frequencyCounter2)){
+    // Checks if letter (which is a key) in frequencyCounter1 is in frequencyCounter2
+    for (let key in frequencyCounter1) {
+        if (!frequencyCounter2[key]) {
             return false;
         }
     }
